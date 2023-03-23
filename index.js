1abe2b94a969013faebccef75349c2a1f69b322b
Cody Cogbill <codeman24@gmail.com>
2023-03-22 19:28:21 -0600

    conrol flow lab done

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(theRide){
+  if (theRide <= 400)
+    return "This one is on me!";
+    else if (theRide > 400 && theRide <2000){
+      return "That will be twenty bucks.";
+    }
+      else if (theRide > 2000 && theRide < 2500){
+        return "I will gladly take your thirty bucks.";}
+        else if (theRide > 2500){
+          return "No can do."
+        }
+      }
+      
+
+
+
+
+
+function ternaryCheckCity(city){
+ if (city === "NYC") {
+  return "Ok, sounds good."
+ }
+ else {
+  return "No go."
+ }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === "generous"){
+    return "Thank you so much."
+  }
+  else if (tip === "not as generous"){
+    return "Thank you."
+  }
+  else {
+    return "Bye."
+  }
 }
